d285cedc96bf676faf332eed7c75efb8de8eeda0
Akash Verma <aakashverma7184@gmail.com>
2023-08-09 19:42:34 +0530

    Initial commit

---
+++ src/server.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+
+
+api = Api(app)
+
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"message": "Hello, Flask-RESTful!"}
+
+
+api.add_resource(HelloWorld, "/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
