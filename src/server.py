168a8f14df87eff2246c894726b53a2cfbacb8b7
Akash Verma <aakashverma7184@gmail.com>
2023-08-12 12:17:12 +0530

    Change controller to take request body

--- src/server.py
+++ src/server.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from .controllers.controller import (
     ListObjectNamesResource,
-    VideoClipResource,
+    GenerateClip,
     S3Resource,
 )
 
@@ -12,7 +12,7 @@ api = Api(app)
 
 
 api.add_resource(ListObjectNamesResource, "/list-object-names")
-api.add_resource(VideoClipResource, "/generate-clip")
+api.add_resource(GenerateClip, "/generate-clip")
 api.add_resource(S3Resource, "/upload/<string:file_name>")
 
 if __name__ == "__main__":
